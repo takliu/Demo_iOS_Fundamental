021f5fb5d2e80cfbebfb526b3c71a2759348251a
Tak Liu <liutak999@gmail.com>
2022-05-24 13:10:36 -0400

    Initial Commit

---
+++ Demo_Fundamental/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo_Fundamental
+//
+//  Created by Chi Tak Liu on 24/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
